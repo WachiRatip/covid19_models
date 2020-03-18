e2b472d4036e1dc49de09d8e04b68b4a5f4d3c5e
WachiRatip <47378511+WachiRatip@users.noreply.github.com>
2020-03-18 08:54:50 +0700

    แก้ powershell script ใหม่

---
+++ build.ps1
@@ -0,0 +1,26 @@
+﻿# build docker image
+docker build -t covid19_python369:1.0.0 .
+
+$command=$args[0]
+if ($command -eq "start") {
+    # run container
+    ## for Windows
+    docker run -p 8888:8888 --name container_covid19 -v ${pwd}\source:/home  covid19_python369:1.0.0
+}
+else {
+    # delete container, image and volume
+    ## stop all containers
+    docker stop $(docker ps -aq)
+
+    ## remove all containers
+    docker rm -f $(docker ps -aq)
+
+    ## remove all docker images
+    #docker image prune -a -f
+
+    ## remove all volume
+    docker volume prune -f 
+}
+
+docker image ls 
+docker ps
